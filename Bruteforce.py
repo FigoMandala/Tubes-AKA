846e342d2f3090d831e9d7ebfee6f61a590150dd
FigoMandala <169130990+FigoMandala@users.noreply.github.com>
2024-12-16 21:42:00 +0700

    Update Bruteforce.py

--- Bruteforce.py
+++ Bruteforce.py
@@ -5,7 +5,7 @@ def brute_force_pattern_search(text, pattern):
     m = len(pattern)
     matches = []
 
-    start_time = time.time()  # Mulai pengukuran waktu
+    start_time = time.time() 
 
     for i in range(n - m + 1):
 
@@ -13,10 +13,10 @@ def brute_force_pattern_search(text, pattern):
         while j < m and text[i + j] == pattern[j]:
             j += 1
         if j == m:  # Jika seluruh pola cocok
-            matches.append(i)  # Simpan indeks kecocokan
+            matches.append(i)  
 
-    end_time = time.time()  # Akhiri pengukuran waktu
-    elapsed_time = (end_time - start_time) * 1000  # Konversi ke milidetik
+    end_time = time.time()  
+    elapsed_time = (end_time - start_time) * 1000 
 
     # Format waktu dalam 0.0000 ms
     print(f"Waktu eksekusi: {elapsed_time:.4f} ms")
